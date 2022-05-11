a5202da4ae7823da28377c33e62bb509071457e3
Artun Akdogan <artun.akdogan@boun.edu.tr>
2022-05-11 17:01:12 +0300

    Add ip location info for client themself

--- practice_app/apps/location_mgr/views.py
+++ practice_app/apps/location_mgr/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+import requests
 
+# External ip-api gateway to get ip location
+def get_ip(ip):
+    res = requests.get(f"http://ip-api.com/json/{ip}")
+    if res.status_code==200:
+        data = res.json()
+        if data["status"]=="success":
+            return str(data)
+        return f"Failed with message: {data['message']}"
+    
+    return f"Failed with code: {res.status_code}"
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    ip = request.META.get('HTTP_X_FORWARDED_FOR')
+    if not ip:
+        ip = request.META.get('REMOTE_ADDR')
+    return HttpResponse(get_ip(ip))
