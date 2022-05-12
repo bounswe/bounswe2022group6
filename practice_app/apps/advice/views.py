5294d3c17fb948e87479fc997c8625836c1b277d
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-05-12 15:43:14 +0300

    Implemented the view for the POST requests.

--- practice_app/apps/advice/views.py
+++ practice_app/apps/advice/views.py
@@ -22,3 +22,21 @@ class api(APIView):
 
         return JsonResponse({"advice_categories":advice_categories}, status=status.HTTP_200_OK)
     
+    def post(self, request):
+    
+        user_age=request.POST["age"]
+        user_sex=request.POST["sex"]
+        user_tobaccoUse=request.POST["tobaccoUse"]
+        user_sexuallyActive=request.POST["sexuallyActive"]
+        
+        r = requests.get(f"https://health.gov/myhealthfinder/api/v3/myhealthfinder.json?lang=en&age={user_age}&sex={user_sex}&tobaccoUse={user_tobaccoUse}&sexuallyActive={user_sexuallyActive}&category=some").json()
+        
+        advice_list=[]
+        
+        i=1
+        
+        for advice in r["Result"]["Resources"]["some"]["Resource"]:
+            advice_list.append({"index":i, "advice_title":advice["MyHFTitle"], "advice_desc":advice["MyHFDescription"].lstrip("<p>").rstrip(" (USPSTF)</p>\r\n").replace("\u2014&nbsp", "")})
+            i += 1
+    
+        return JsonResponse({"advice_list":advice_list}, status=status.HTTP_200_OK)
