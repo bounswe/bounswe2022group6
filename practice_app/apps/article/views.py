c183088c21de844ff3a9e3fc9c46aad50afe7b2d
Ali Kaan Biber <74202057+Aliqaan@users.noreply.github.com>
2022-05-14 20:45:28 +0300

    removed the localhost dependency from article app

--- practice_app/apps/article/views.py
+++ practice_app/apps/article/views.py
@@ -12,26 +12,28 @@ from .models import Article
 
 # Create your views here.
 
-
+#This function defines the GET and POST operations for my api choice
 @api_view(["GET","POST"])
 def api(req):
     if req.method == "GET":
+
+        #Get all the articles in database and process them to produce a GET response
         articles = serializers.serialize( "python", Article.objects.all(),fields = ("Article_title","Article_summary","Article_authors","Article_link") )
         response ={}
         for i in range(len(articles)):
             response["article"+str(i+1)] = articles[i]["fields"]
-
         return Response(response,status=status.HTTP_200_OK)
 
     if req.method == "POST":
         
+        #First try to catch possible errors with post body and produce an error response
         try:
             
             subject = req.POST["subject"]
             noOfArticles = req.POST["numberOfArticles"]
 
         except Exception as e:
-            return Response("Missing Input",status=status.HTTP_400_BAD_REQUEST)
+            return Response("Bad Input",status=status.HTTP_400_BAD_REQUEST)
 
         try:
             int(noOfArticles)
@@ -44,14 +46,17 @@ def api(req):
         elif(int(noOfArticles)<= 0):
             return Response("numberOfArticles field cannot be non-positive",status=status.HTTP_400_BAD_REQUEST)
 
+        #If post body is succesfull then make a search based on given inputs using external api: arXiv (https://arxiv.org/help/api/basics)
         parameters = {"search_query":"all:"+subject,"max_results":noOfArticles}
         data = requests.get("http://export.arxiv.org/api/query",params = parameters)
 
+        #Process the data to find necessary information about article search result
         ArticleInfoList = findInfo(data,noOfArticles)
         
         newData = {}
         numberOfArticlesInDb = len(Article.objects.all())
 
+        #For each result create the Article object to store and produce the api POST request response which is the found article attributes.
         for i in range(len(ArticleInfoList)):
 
             authors = ""
@@ -67,13 +72,15 @@ def api(req):
 
         return Response(newData,status=status.HTTP_200_OK)
 
+#Renders the index page for UI
 def index(req):
     
     return render(req, "index.html")
 
+#This function sends a request to api and uses it response to render the search history in html
 def list(req):
-    data = requests.get("http://127.0.0.1:8000/article/api")
-    
+    apiUrl = "http://"+req.get_host()+"/article/api"
+    data = requests.get(apiUrl)
     data = data.text
     data =json.loads(data)
     Information = []
@@ -92,14 +99,16 @@ def list(req):
     else:
         return render(req,"list.html",{"ArticleInfoList":Information,"isEmpty":False})
 
+#This function sends a request to api and uses it response to render the search results in html based on the user input taken by user interface
 def search(req):
     if req.method == "POST":
 
         subject = req.POST["subject"]
         noOfArticles = req.POST["numberOfArticles"]    
         
+        apiUrl = "http://"+req.get_host()+"/article/api"
         parameters = {"subject":subject,"numberOfArticles":noOfArticles}
-        data = requests.post("http://127.0.0.1:8000/article/api",data=parameters)
+        data = requests.post(apiUrl,data=parameters)
 
         data = data.text
 
@@ -122,6 +131,7 @@ def search(req):
     else :
         return render(req, "index.html")
 
+#This function helps processing the external api's response to a dictionary format which is easier to work with.
 def findInfo(data,noOfArticles):
 
     dataString = data.text
@@ -164,6 +174,7 @@ def findInfo(data,noOfArticles):
         ArticleInfoList.append((title,summary,authors,articleLink,i+1))
     return ArticleInfoList
 
+#This function is activated by UI and it deletes the article database therefore article history.
 def deleteHistory(req):
     if req.method == "POST":
         try:
@@ -171,7 +182,3 @@ def deleteHistory(req):
             return HttpResponseRedirect("../article")
         except Exception as e:
             return Response("Could not performed deletion operation",status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
