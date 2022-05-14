ce4daf7c07f0dd87d435d8e38494375cdd54b947
alika <alikaan.antalya@gmail.com>
2022-05-14 15:50:52 +0300

    minor corrections in code

--- practice_app/practice_app/settings.py
+++ practice_app/practice_app/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
