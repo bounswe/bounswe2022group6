a20d833da2176e05b2a17345040974ed4b7bf87b
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-05-16 22:31:45 +0300

    Advice home page now displays the statistics of the past users. The GET requests additionally return the user statistics. The relevant data is queried from the database.

--- practice_app/apps/advice/views.py
+++ practice_app/apps/advice/views.py
@@ -22,8 +22,34 @@ class api(APIView):
             advice_categories.add(advice_category["Title"])
             
         advice_categories = list(advice_categories)
+        
+        male_count=AdviceUser.objects.filter(sex="male").count()
+        female_count=AdviceUser.objects.filter(sex="female").count()
+        
+        smoker_count=AdviceUser.objects.filter(tobaccoUse=1).count()
+        non_smoker_count=AdviceUser.objects.filter(tobaccoUse=0).count()
+        
+        active_count=AdviceUser.objects.filter(sexuallyActive=1).count()
+        inactive_count=AdviceUser.objects.filter(sexuallyActive=0).count()
+        
+        all_ages= []
+        
+        all_users=AdviceUser.objects.values("age")
+        for usage in all_users:
+            all_ages.append([usage["age"]])
 
-        return JsonResponse({"advice_categories":advice_categories}, status=status.HTTP_200_OK)
+        #smoker_count = len(AdviceUser.objects.filter())
+
+        return JsonResponse({"advice_categories":advice_categories,
+                             "statistics":{"male_count":male_count,
+                                           "female_count":female_count,
+                                           "smoker_count":smoker_count,
+                                           "non_smoker_count":non_smoker_count,
+                                           "active_count":active_count,
+                                           "inactive_count":inactive_count,
+                                           "all_user_ages":all_ages
+                                          }
+                            }, status=status.HTTP_200_OK)
     
     def post(self, request):
     
@@ -40,10 +66,10 @@ class api(APIView):
         advice_list=[]
         
         i=1
-        
-        for advice in r["Result"]["Resources"]["some"]["Resource"]:
-            advice_list.append({"index":i, "advice_title":advice["MyHFTitle"], "advice_desc":advice["MyHFDescription"].lstrip("<p>").rstrip(" (USPSTF)</p>\r\n").replace("\u2014&nbsp", "")})
-            i += 1
+        if "Resources" in r["Result"]:
+            for advice in r["Result"]["Resources"]["some"]["Resource"]:
+                advice_list.append({"index":i, "advice_title":advice["MyHFTitle"], "advice_desc":advice["MyHFDescription"].lstrip("<p>").rstrip(" (USPSTF)</p>\r\n").replace("\u2014&nbsp", "")})
+                i += 1
             
         return JsonResponse({"advice_list":advice_list}, status=status.HTTP_200_OK)
         
