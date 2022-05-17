db031d0bd2f4de29fb850bb8e7ec8539c34b9f02
Hakan <hakan.balik.95@gmail.com>
2022-05-17 21:47:25 +0300

    Finished API implementations.

--- practice_app/apps/news/urls.py
+++ practice_app/apps/news/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("", views.news_home, name="newsHome"),
+    path("home", views.news_home, name="news_home"),
+    path("newsWithQuery", views.news_with_query, name="newsWithQuery"),
+    path("newsWithoutQuery", views.news_without_query, name="newsWithoutQuery")
 ]
