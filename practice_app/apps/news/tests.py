a475178e7f6c5e4a9f4a6af848d3e572283719bb
Hakan <hakan.balik.95@gmail.com>
2022-05-19 19:13:41 +0300

    Added test cases for news app.

--- practice_app/apps/news/tests.py
+++ practice_app/apps/news/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+class NewsTest(TestCase):
+
+    def test_news_index(self):
+        response = self.client.get('/news/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response ,'news_index.html')
+
+    def test_news_list_without_query(self):
+        response = self.client.get('/news/newsWithoutQuery')
+        self.assertEquals(response.status_code, 200)
+    
+    def test_news_list_with_query(self):
+        response = self.client.post('/news/newsWithQuery', {"country": "Turkey", "start_date":"","keyword": "corona", "sort": "popularity"})
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response ,'news_list.html')
