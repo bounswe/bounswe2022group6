02d43c061447901db14d11c7024a626b20597296
yasirdikbas <yasir.dikbas@boun.edu.tr>
2022-05-20 15:16:26 +0300

    my changes

--- practice_app/apps/Hospi/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-# Create the form class.
-class HospitalCreationForm(ModelForm):
-    class Meta:
-        model = Hospital
-        fields = "__all__"
