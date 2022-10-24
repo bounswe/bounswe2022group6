f8af78f0ca98381ac78c825c355239228a324f81
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-10-24 20:34:57 +0300

    Fix url bug and database procedure errors

--- Application/Backend/src/accmgr/views.py
+++ Application/Backend/src/accmgr/views.py
@@ -1,22 +1,25 @@
 from rest_framework.views import APIView
+from django.http import JsonResponse
 from datetime import date
 from hashlib import sha256
-from models import *
+from .models import *
 
 class RegisterUser(APIView):
 
     def post(self, req):
-        username = req.POST.get("username", None).strip()
-        email = req.POST.get("email", None).lower().strip()
-        gender = req.POST.get("gender", None).upper().strip()
-        birth_year = int(req.POST.get("birth_year", None).strip())
-        birth_month = int(req.POST.get("birth_month", None).strip())
-        birth_day = int(req.POST.get("birth_day", None).strip())
-        birth_date = date(birth_year, birth_month, birth_day)
-        password_hash = sha256(req.POST.get("password", None))
+        try:
+            username = req.POST.get("username", None).strip()
+            email = req.POST.get("email", None).lower().strip()
+            gender = req.POST.get("gender", None).upper().strip()
+            birth_year = int(req.POST.get("birth_year", None).strip())
+            birth_month = int(req.POST.get("birth_month", None).strip())
+            birth_day = int(req.POST.get("birth_day", None).strip())
+            birth_date = date(birth_year, birth_month, birth_day)
+            password_hash = sha256(req.POST.get("password", None).encode('UTF-8')).hexdigest()
 
-        new_user = User(username, email, password_hash, birth_date, gender)
+            new_user = User(username=username, email=email, password_hash=password_hash, birth_date=birth_date, gender=gender)
 
-        new_user.save()
-
-# Create your views here.
+            new_user.save()
+            return JsonResponse({"info": "Success!", "user": new_user.userID})
+        except Exception as e:
+            return JsonResponse({"info": "Fail!", "error": str(e)})
--- Application/Backend/src/urls.py
+++ Application/Backend/src/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from src.accmgr.views import RegisterUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterUser.as_view(), name="register"),
 ]
