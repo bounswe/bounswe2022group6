6f276910ce470c14771893466974981acbeec270
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-10-24 19:35:38 +0300

    Added user User model

--- Application/Backend/src/accmgr/models.py
+++ Application/Backend/src/accmgr/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator, MinLengthValidator
 
-# Create your models here.
+class User(models.Model):
+
+    userID = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=16, validators=[RegexValidator(r'^[\w-]*$'), MinLengthValidator(3)])
+    email = models.EmailField(unique=True, blank=False)
+    password_hash = models.CharField(max_length=256)
+    birth_date = models.DateField()
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', "Other"),
+        ("D", "Do not want to specify")
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
