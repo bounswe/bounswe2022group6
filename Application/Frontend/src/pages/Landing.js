0f34242363d7851a98db396c72425fa7bd49d20f
hakan.balik <hakan.balik@leanscale.com>
2022-10-26 18:42:36 +0300

    Added Landing page and removed unnecessary code blocks & assets.

---
+++ Application/Frontend/src/pages/Landing.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../App.css'
+import BackgroundImage from '../assets/boun_1.png'
+
+export default function Landing() {
+    return (
+        <header style={ HeaderStyle }>
+            <div class = "center">
+                <h1 class="main-title text-center">Welcome to MediShare</h1>
+                <p class="main-para text-center">Please Register or Login</p>
+                <div class="buttons text-center">
+                    <Link to="/login">
+                        <button class="primary-button" id="home_button"> <span>log in </span></button>
+                    </Link>
+                    <Link to="/register">
+                        <button class="primary-button" id="home_button"><span>register </span></button>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+const HeaderStyle = {
+    background: `url(${BackgroundImage})`,
+    width: "100%",
+    height: "100vh",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
