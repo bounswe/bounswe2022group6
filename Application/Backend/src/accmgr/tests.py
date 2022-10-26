a9081707e8225b37325411fa6e7c14e44c6c739e
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-10-26 05:05:57 +0300

    Added username unit tests
    The username unit tests test the character count bounds and the
    additional special character constraints of the username field.

--- Application/Backend/src/accmgr/tests.py
+++ Application/Backend/src/accmgr/tests.py
@@ -1,19 +1,40 @@
-from django.test import TransactionTestCase
+from django.test import TestCase, Client
 from .models import *
 import json
 
-class TestViews(TransactionTestCase):
+class RegistrationTest(TestCase):
 
-    def test_register_post(self):
-        message = json.loads(self.client.post('/register', { "username": "user1", "email": "local@user.com",
-                "password": "passWord!", "gender":"m", "birth_day":"06", "birth_month":"10", "birth_year":"1970"}).content)
-        self.assertEqual(message['info'], 'success')
-        message = json.loads(self.client.post('/register', { "username": "user2@user.com", "email": "local@user.com",
-                "password": "passWord!", "gender":"M", "birth_day":"06", "birth_month":"10", "birth_year":"1970"}).content)
-        self.assertEqual(message['info'], 'failure')
-        message = json.loads(self.client.post('/register', { "username": "user3", "email": "local@user.com",
-                "password": "passWord!", "gender":"q", "birth_day":"06", "birth_month":"10", "birth_year":"1970"}).content)
-        self.assertEqual(message['info'], 'failure')
-        self.assertEqual(User.objects.count(), 1)
+    def setUp(self):
+        self.client = Client()
 
-# Create your tests here.
+    def test_fields(self):
+        response = self.client.post('/register/', { "username": "mark", "email": "mark.zucky@facadeledger.com",
+                "password": "passWord!", "gender":"m", "birth_day":"06", "birth_month":"10"})
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["info"], 'user registration failed')
+        self.assertEqual(response_content["error"], "{'form_data': ['Missing form data.']}")
+        self.assertEqual(response.status_code, 400)
+
+    def test_bad_username(self):
+        response = self.client.post('/register/', { "username": "mark.zucky", "email": "mark.zucky@facadeledger.com",
+                "password": "passWord!", "gender":"m", "birth_day":"06", "birth_month":"10", "birth_year":"1970"})
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["info"], 'user registration failed')
+        self.assertEqual(response_content["error"], "{'username': ['Enter a valid value.']}")
+        self.assertEqual(response.status_code, 400)
+
+    def test_long_username(self):
+        response = self.client.post('/register/', { "username": "markzuckyzuckerson", "email": "mark.zucky@facadeledger.com",
+                "password": "passWord!", "gender":"m", "birth_day":"06", "birth_month":"10", "birth_year":"1970"})
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["info"], 'user registration failed')
+        self.assertEqual(response_content["error"], "{'username': ['Ensure this value has at most 16 characters (it has 18).']}")
+        self.assertEqual(response.status_code, 400)
+
+    def test_short_username(self):
+        response = self.client.post('/register/', { "username": "mk", "email": "mark.zucky@facadeledger.com",
+                "password": "passWord!", "gender":"m", "birth_day":"06", "birth_month":"10", "birth_year":"1970"})
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["info"], 'user registration failed')
+        self.assertEqual(response_content["error"], "{'username': ['Ensure this value has at least 3 characters (it has 2).']}")
+        self.assertEqual(response.status_code, 400)
