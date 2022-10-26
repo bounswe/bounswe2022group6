78fd56f2ffab4649bee70c2cf9396bb0930e2d23
hakan.balik <hakan.balik@leanscale.com>
2022-10-27 02:30:43 +0300

    Changed css, removed required field for edit, removed select option on gender.

--- Application/Frontend/src/pages/Profile.js
+++ Application/Frontend/src/pages/Profile.js
@@ -9,43 +9,45 @@ export default function ProfilePage() {
             <form >
             <p>
                         <label>Username</label><br/>
-                        <input type="text" name="first_name" required />
+                        <input type="text" name="first_name" />
                     </p>
+                    <br/>
                     <p>
                         <label>Name</label><br/>
-                        <input type="text" name="first_name" required />
+                        <input type="text" name="first_name" />
                     </p>
+                    <br/>
                     <p>
                         <label>Surname</label><br/>
-                        <input type="text" name="first_name" required />
+                        <input type="text" name="first_name" />
                     </p>
+                    <br/>
                     <p>
                         <label>Email address</label><br/>
-                        <input type="email" name="email" required />
+                        <input type="email" name="email" />
                     </p>
+                    <br/>
                     <p>
                         <label>Password</label><br/>
-                        <input type="password" name="password" requiredc />
+                        <input type="password" name="password" />
                     </p>
+                    <br/>
                     <p>
                         <label>Date of Birth</label><br/>
-                        <input type="date" name="date" requiredc />
+                        <input type="date" name="date" />
                     </p>
-
+                    <br/>
                     <label htmlFor="genderSelect">Gender</label><br/>
                     
                     <select id="genderSelect" >
-                    <option value="">select</option>
+                    <option value="do not want to specify">Do not want to specify</option>
                     <option value="male">Male</option> 
                     <option value="female">Female</option>
                     <option value="other">Other</option>
-                    <option value="do not want to specify">Do not want to specify</option>
                     </select><br/><br/>
-                    <button  className="edit-button">
+                    <button  id="submit_btn" type="submit" >
                         Edit
                     </button>
-                
-                    
                 </form>
                 <div> 
             <Link to="/home">
