dfcbfcc046cfc596dcd46e946089cd84a4ad3da9
hakan.balik <hakan.balik@leanscale.com>
2022-10-27 02:24:56 +0300

    Corrected css, typos, removed select option on gender.

--- Application/Frontend/src/pages/Register.js
+++ Application/Frontend/src/pages/Register.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 
 import '../App.css'
-import BackgroundImage from '../assets/boun_2.png'
+import BackgroundImage from '../assets/boun_3.png'
 
 
 export default function SignUpPage() {
@@ -11,46 +11,46 @@ export default function SignUpPage() {
     return (
         <header style={ HeaderStyle }>
             <div className="center">
-                <h2>Join us</h2>
-                <h5>Create your personal account</h5>
+                <h2 class="h2Register">Register to MediShare</h2>
+                <h5 class="h5Register">Create your personal account</h5>
                 <form action="/home" >
                     <p>
                         <label>Username</label><br/>
                         <input type="text" name="first_name" required />
                     </p>
+                    <br/>
                     <p>
                         <label>Email address</label><br/>
                         <input type="email" name="email" required />
                     </p>
+                    <br/>
                     <p>
                         <label>Password</label><br/>
-                        <input type="password" name="password" requiredc />
+                        <input type="password" name="password" required />
                     </p>
+                    <br/>
                     <p>
-                        <label>Date of Birth</label><br/>
-                        <input type="date" name="date" requiredc />
+                        <label>Date of Birth</label><br/> 
+                        <input type="date" name="date" required />
                     </p>
-
+                    <br/>
                     <label htmlFor="genderSelect">Gender</label><br/>
                     
                     <select id="genderSelect" >
-                    <option value="">select</option>
+                    <option value="do not want to specify" >Do not want to specify</option>
                     <option value="male">Male</option> 
                     <option value="female">Female</option>
                     <option value="other">Other</option>
-                    <option value="do not want to specify">Do not want to specify</option>
                     </select><br/><br/>
                     <p>
                         <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
                     </p>
                     <p>
-                        <button id="sub_btn" type="submit">Register</button>
+                        <button id="submit_btn" type="submit">Register</button>
                     </p>
-                    
-                </form>
-                <footer>
+                    <br/>
                     <p><Link to="/">Back to Homepage</Link>.</p>
-                </footer>
+                </form>
             </div>
         </header>
     )
