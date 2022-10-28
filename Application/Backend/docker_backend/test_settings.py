34a448b91fd44adcb707f270da183a0f0a5b7188
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-10-28 20:27:28 +0300

    Updated User model class.
    The User model class is updated and it is noe RegisteredUser. It
    inherits from AbstractUser to enable using TokenAuthentication. The
    birth_date and gender fields now have default values as they are not
    enforced. We don't want them to be null as they are necessary for
    registration.

--- Application/Backend/docker_backend/test_settings.py
+++ Application/Backend/docker_backend/test_settings.py
@@ -120,3 +120,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.IsAuthenticated',
+    ),
+
+}
+
+AUTH_USER_MODEL = "accmgr.RegisteredUser"
--- Application/Backend/src/settings.py
+++ Application/Backend/src/settings.py
@@ -128,3 +128,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+               'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+                'rest_framework.permissions.AllowAny',
+    ),
+
+}
+
+AUTH_USER_MODEL = "accmgr.RegisteredUser"
