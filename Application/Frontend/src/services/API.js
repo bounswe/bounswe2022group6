b51d5ec8e7fc7ed764d26923f6e03d4842c6910f
Ali Kaan Biber <alikaan.antalya@gmail.com>
2022-10-28 13:03:52 +0300

    moved the endpoint call to another file for later integrity

---
+++ Application/Frontend/src/services/API.js
@@ -0,0 +1,39 @@
+
+// register endpoint
+async function register(data) {
+
+    var formData = new FormData()
+
+    for (var key in data) {
+        console.log(key, data[key])
+        if (key === "gender") {
+            formData.append(key, data[key][0])
+        }
+        else if (key !== "date")
+            formData.append(key, data[key])
+        else {
+            const splittedString = data[key].split("-")
+            formData.append("birth_year", splittedString[0])
+            formData.append("birth_month", splittedString[1])
+            formData.append("birth_day", splittedString[2])
+        }
+    }
+    
+    const requestOptions = {
+        method: "POST",
+        header: "Content-Type: multipart/form-data",
+        body: formData
+    }
+    console.log("Sending Request")
+    
+    const response = await fetch("/register", requestOptions)
+    const resMessage = await response.json()
+    if (response.status === 400) {
+        console.log(resMessage["error"])
+        return resMessage["error"]
+    } else {
+        return null
+    }
+}
+
+export default register
