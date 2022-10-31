0649ebc5ea39480657d47c3f6ba990c9fa67acf4
hakan.balik <hakan.balik@leanscale.com>
2022-10-31 22:18:40 +0300

    Changed localhost URL to window.location and introduced localstorage for auth_token.

--- Application/Frontend/src/services/Login_API.js
+++ Application/Frontend/src/services/Login_API.js
@@ -1,5 +1,3 @@
-const URL =  "http://localhost:8000"
-
 async function login(data) {
 
     var formData = new FormData()
@@ -13,13 +11,14 @@ async function login(data) {
         body: formData
     }
     
-    const response = await fetch( URL +'/login', requestOptions)
+    const response = await fetch(window.location.href.replace(":3000", ":8000"), requestOptions)
     const resMessage = await response.json()
     console.log(resMessage)
     if (response.status === 400 || response.status === 401) {
         console.log(resMessage["error"])
         return resMessage["error"]
     } else {
+        window.localStorage.setItem("auth_token",resMessage["token"])
         return null
     }
 }
