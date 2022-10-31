ee166cc8170944ebde299abd0c03a6038582f2e7
= <=>
2022-10-31 18:59:06 +0300

    signup page implemented

--- Application/Mobile/App.js
+++ Application/Mobile/App.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  useColorScheme,
-  View,
 } from 'react-native';
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import { LoginPage } from './pages/authantication/Login';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SignUpPage } from './pages/authantication/Signup';
+import { Text } from "@react-native-material/core";
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const Stack = createNativeStackNavigator();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View>
-            <LoginPage />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={LoginPage}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={SignUpPage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer >
+    </>
   );
 };
 
