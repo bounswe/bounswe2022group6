5b3c6716cfe12eb9f0dfdd586372ab61c941f85f
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-11-08 02:04:28 +0300

    Add locmgr initialization and required models for location data on the database

---
+++ Application/Backend/src/locmgr/models.py
@@ -0,0 +1,76 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Cities(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.ForeignKey('States', models.DO_NOTHING)
+    state_code = models.CharField(max_length=255)
+    country = models.ForeignKey('Countries', models.DO_NOTHING)
+    country_code = models.CharField(max_length=2)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    flag = models.IntegerField()
+    wikidataid = models.CharField(db_column='wikiDataId', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'cities'
+
+
+class Countries(models.Model):
+    name = models.CharField(max_length=100)
+    iso3 = models.CharField(max_length=3, blank=True, null=True)
+    numeric_code = models.CharField(max_length=3, blank=True, null=True)
+    iso2 = models.CharField(max_length=2, blank=True, null=True)
+    phonecode = models.CharField(max_length=255, blank=True, null=True)
+    capital = models.CharField(max_length=255, blank=True, null=True)
+    currency = models.CharField(max_length=255, blank=True, null=True)
+    currency_name = models.CharField(max_length=255, blank=True, null=True)
+    currency_symbol = models.CharField(max_length=255, blank=True, null=True)
+    tld = models.CharField(max_length=255, blank=True, null=True)
+    native = models.CharField(max_length=255, blank=True, null=True)
+    region = models.CharField(max_length=255, blank=True, null=True)
+    subregion = models.CharField(max_length=255, blank=True, null=True)
+    timezones = models.TextField(blank=True, null=True)
+    translations = models.TextField(blank=True, null=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, blank=True, null=True)
+    emoji = models.CharField(max_length=191, blank=True, null=True)
+    emojiu = models.CharField(db_column='emojiU', max_length=191, blank=True, null=True)  # Field name made lowercase.
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField()
+    flag = models.IntegerField()
+    wikidataid = models.CharField(db_column='wikiDataId', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'countries'
+
+
+class States(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Countries, models.DO_NOTHING)
+    country_code = models.CharField(max_length=2)
+    fips_code = models.CharField(max_length=255, blank=True, null=True)
+    iso2 = models.CharField(max_length=255, blank=True, null=True)
+    type = models.CharField(max_length=191, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8, blank=True, null=True)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField()
+    flag = models.IntegerField()
+    wikidataid = models.CharField(db_column='wikiDataId', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'states'
+
