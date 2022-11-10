95eef7740426402f6f31d9da595495af70851dcc
İhsan Mert Atalay <imrtatalay_111@hotmail.com>
2022-11-10 21:50:55 +0300

    label seletion panel and colors designed dynamically

--- Application/Frontend/src/pages/Home.js
+++ Application/Frontend/src/pages/Home.js
@@ -6,7 +6,8 @@ import { useHistory } from "react-router-dom";
 import { useState } from "react";
 import MessageBox from "../components/MessageBox";
 import { ImArrowUp, ImArrowDown } from "react-icons/im";
-import { BsSearch } from "react-icons/bs";
+import { BsSearch, BsFilter } from "react-icons/bs";
+import { FiDelete } from "react-icons/fi";
 
 const posts = [
   {
@@ -79,23 +80,40 @@ const labels = [
   { name: "Other", backgroundColor: "Black", color: "white" },
   { name: "Article", backgroundColor: "Blue", color: "white" },
   { name: "Healthy Life", backgroundColor: "Green", color: "yellow" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
-  { name: "Medication", backgroundColor: "red", color: "white" },
+  { name: "Wound Care", backgroundColor: "yellow", color: "black" },
+  { name: "Injury", backgroundColor: "lightblue", color: "black" },
+  { name: "Mental Health", backgroundColor: "gray", color: "white" },
+  { name: "Parasite Concern", backgroundColor: "orange", color: "white" },
+  { name: "Rabies Concern", backgroundColor: "purple", color: "white" },
+  { name: "Pregnancy", backgroundColor: "lightgreen", color: "white" },
+  {
+    name: "Skin issues/Rashes/Freckles/Moles",
+    backgroundColor: "darkgray",
+    color: "white",
+  },
+  { name: "Cardiac", backgroundColor: "darkgreen", color: "white" },
+  {
+    name: "Mouth/Gums/Throat/Cheeks",
+    backgroundColor: "Salmon",
+    color: "white",
+  },
+  { name: "Eyes", backgroundColor: "Lime", color: "white" },
+  { name: "Illness", backgroundColor: "pink", color: "indigo" },
+  { name: "Digestion/Stomach/Bowels", backgroundColor: "teal", color: "white" },
+  {
+    name: "Bones/Joints/Ligaments",
+    backgroundColor: "SkyBlue",
+    color: "white",
+  },
+  { name: "Help", backgroundColor: "palegreen", color: "white" },
+  { name: "Advice", backgroundColor: "PaleTurquoise", color: "white" },
 ];
-const addedlabels = [];
 export default function Home() {
   let history = useHistory();
 
   const [isLoggedout, setLoggedout] = useState(false);
-  const [mert, setMert] = useState(0);
+  const [dummy, setdummy] = useState(0);
+  const [addedlabels, setaddedlabels] = useState([]);
 
   function handleClick(event) {
     event.preventDefault();
@@ -110,8 +128,10 @@ export default function Home() {
   }
 
   const addlabel = (label) => {
-    addedlabels.push(label);
-    setMert(mert + 1);
+    if (addedlabels.indexOf(label) < 0) {
+      addedlabels.push(label);
+      setdummy(dummy + 1);
+    }
   };
 
   const vote = (mypost, direction) => {
@@ -136,8 +156,20 @@ export default function Home() {
       mypost.voted = "";
       mypost.score++;
     }
-    setMert(mert + 1);
+    setdummy(dummy + 1);
     console.log(mypost.voted);
+  };
+
+  const clearfilters = () => {
+    setaddedlabels([]);
+  };
+  const removeLabel = (mylabel) => {
+    const index = addedlabels.indexOf(mylabel);
+    if (index > -1) {
+      // only splice array when item is found
+      addedlabels.splice(index, 1); // 2nd parameter means remove one item only
+      setdummy(dummy + 1);
+    }
   };
 
   return (
@@ -262,8 +294,12 @@ export default function Home() {
             </div>
           ))}
         </div>
-        <div className="rightSide" style={{ width: "30%", height: "700px", backgroundColor: "" }}>
+        <div
+          className="rightSide"
+          style={{ width: "30%", height: "700px", backgroundColor: "" }}
+        >
           <div
+            className="searchBar"
             style={{
               backgroundColor: "white",
               marginBlockEnd: "20px",
@@ -281,46 +317,70 @@ export default function Home() {
                 width: "60%",
                 height: "40px",
                 fontSize: "19px",
-                borderRadius: "0px",
+                borderRadius: "5px 0px 0px 5px",
               }}
             />
             <BsSearch
+              className={styles.BsSearch}
               style={{
                 border: "1px solid gray",
                 height: "40px",
                 borderLeft: "0px",
                 padding: "0px 3px",
                 width: "25px",
+                borderRadius: "0px 5px 5px 0px",
               }}
             />
-            <div class={styles.dropdown}>
-              <button class={styles.dropbtn}>Sort By</button>
-              <div class={styles.dropdowncontent}>
-                <a href="#">Time</a>
-                <a href="#">Rating</a>
-                <a href="#">Relevance</a>
+            <div className={styles.dropdown}>
+              <button className={styles.dropbtn}>Sort By</button>
+              <div className={styles.dropdowncontent}>
+                <p href="#">Time</p>
+                <p href="#">Rating</p>
+                <p href="#">Relevance</p>
               </div>
             </div>
           </div>
 
           <div
+            className="labelsSide"
             style={{
               display: "inline-block",
               padding: "10px",
               backgroundColor: "white",
               width: "100%",
+              borderRadius: "5px",
             }}
           >
-            <h1>Labels</h1>
+            <h3
+              style={{
+                display: "flex",
+                alignContent: "left",
+                color: "#383838",
+              }}
+            >
+              Labels
+            </h3>
             <input
               type="text"
               placeholder="Search For Label.."
-              style={{ border: "1px solid gray", marginBlockEnd: "5px" }}
+              style={{
+                border: "1px solid gray",
+                marginBlockEnd: "5px",
+                width: "100%",
+              }}
             />
-            <div style={{ height: "200px", overflow: "auto" }}>
+            <div
+              style={{
+                height: "100px",
+                overflow: "auto",
+                display: "flex",
+                flexWrap: "wrap",
+              }}
+            >
               {labels.map((label) => (
                 <div onClick={() => addlabel(label)}>
                   <p
+                    className={styles.filter}
                     style={{
                       background: label.backgroundColor,
                       color: label.color,
@@ -336,12 +396,23 @@ export default function Home() {
                 </div>
               ))}
             </div>
-            <hr />
+            <hr style={{ margin: "5px" }} />
             <div style={{ textAlign: "left" }}>
-              {addedlabels.length === 0 && <div style={{fontStyle: "italic",color:"gray"}}> add label to filter</div>}
+              {addedlabels.length === 0 && (
+                <div style={{ fontStyle: "italic", color: "gray" }}>
+                  {" "}
+                  add label to filter
+                </div>
+              )}
+              {addedlabels.length > 0 && (
+                <div style={{ color: "gray" }}>Added Labels</div>
+              )}
+
               {addedlabels.map((label) => (
                 <div
+                  className={styles.filter}
                   style={{ display: "flex0,justify-content: space-between;" }}
+                  onClick={() => removeLabel(label)}
                 >
                   <p
                     style={{
@@ -354,18 +425,64 @@ export default function Home() {
                       float: "left",
                     }}
                   >
-                    {" "}
                     {label.name}
+                    <small
+                      style={{
+                        fontSize: "11px",
+                        alignItems: "center",
+                        filter: "brightness(1.3)",
+                        background: label.backgroundColor,
+                        borderRadius: "10px",
+                        padding: "0px 3px",
+                        position: "relative",
+                        bottom: "3px",
+                        left: "5px",
+                      }}
+                    >
+                      x
+                    </small>
                   </p>
                 </div>
               ))}
+              <div style={{ float: "right", display: "flex" }}>
+                <div
+                  className={styles.filter}
+                  style={{
+                    background: "palegreen",
+                    color: "#333333",
+                    border: "1px solid gray",
+                    padding: "2px 10px",
+                    borderRadius: "5px",
+                    margin: " 3px",
+                    display: "flex",
+                    alignItems: "center",
+                    fontSize: "larger",
+                  }}
+                >
+                  Filter <BsFilter />
+                </div>
+                <div
+                  className={styles.filter}
+                  style={{
+                    background: "PaleTurquoise",
+                    border: "1px solid gray",
+                    color: "#333333",
+                    padding: "2px 10px",
+                    borderRadius: "5px",
+                    margin: " 3px",
+                    display: "flex",
+                    alignItems: "center",
+                    fontSize: "larger",
+                  }}
+                  onClick={clearfilters}
+                >
+                  Clear
+                  <FiDelete />
+                </div>
+              </div>
             </div>
           </div>
-          <div className={styles.createpost}>
-            Create Post
-
-            </div>          
-
+          <div className={styles.createpost}>Create Post</div>
         </div>
       </div>
     </div>
