b3806e3953581eab4c63ee89b36bb0efdacd4c21
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-11-28 15:49:32 +0300

    Created Content Model
    The content model is created in accordance with the fields specified
    in the related GitHub issue. It includes the fields:
    - contentID
    - owner
    - description
    - mentioned_users
    - vote_count
    - created_at

--- Application/Backend/src/contmgr/models.py
+++ Application/Backend/src/contmgr/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from src.accmgr.models import *
 
-# Create your models here.
+class Content(models.Model):
+
+    owner = models.ForeignKey(RegisteredUser, on_delete=models.CASCADE, blank=False, null=False)
+    description = models.CharField(max_length=10000, blank=False, null=False)
+    vote_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now())
+    mentioned_users = models.ManyToManyField(RegisteredUser, related_name='mentioned_by_%(class)s', blank=True)
