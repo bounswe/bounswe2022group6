38504c15ba5be20b03e7234445dd8579b32051da
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-02 22:58:42 +0300

    Added as_dict() method to Post Model
    Added as_dict() method to the Post model class to represent the model
    as a dictionary for JSON serialization.

--- Application/Backend/src/contmgr/models.py
+++ Application/Backend/src/contmgr/models.py
@@ -38,6 +38,23 @@ class Post(Content):
     is_marked_nsfw = models.BooleanField(default=False)
     labels = models.ManyToManyField("Label", related_name='labelled_posts', blank=True)
 
+    def as_dict(self):
+        return {
+            "postID" : self.postID,
+            "owner" : {"userID": self.owner.userID, "username": self.owner.username},
+            "description" : self.description,
+            "vote_count" : self.vote_count,
+            "created_at_date" : self.created_at.strftime("%d.%m.%Y"),
+            "created_at_time" : self.created_at.strftime("%H.%M.%S"),
+            "title" : self.title,
+            "type" : self.type,
+            "location" : self.location,
+            "imageURL" : self.imageURL,
+            "is_marked_nsfw" : self.is_marked_nsfw,
+            "labels" : [{"labelID": label.labelID, "labelName": label.labelName} for label in self.labels.all()],
+            "mentioned_users" : [{"userID": user.userID, "username": user.username} for user in self.mentioned_users.all()]
+        }
+
 class Label(models.Model):
 
     labelID = models.AutoField(primary_key=True)
