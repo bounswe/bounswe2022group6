ac2019852183104deda80fb0df2cd09b39e9fcdc
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-03 02:56:05 +0300

    Search Post Functionality Added
    Post searching functionality is implemented. It supports searching by
    multiple keywords and multiple labels.
    
    It returns the posts that contain all the keywords and all the labels.
    
    The data returned include all the fields of the post model.

--- Application/Backend/src/contmgr/views.py
+++ Application/Backend/src/contmgr/views.py
@@ -10,3 +10,33 @@ class Labels(APIView):
         for label in labels:
             data["labels"].append(label.as_dict())
         return JsonResponse(data, status=200, safe=False)
+
+class SearchPost(APIView):
+
+    def get(self, req):
+
+        keywords = req.GET.get("keyword", None)
+        labels = req.GET.getlist("label", None)
+        
+        if keywords is None and labels is None:
+            return JsonResponse({"info":"search failed", "error": "no keyword or label provided"}, status=400)
+
+        posts = Post.objects.all()
+
+        if keywords is not None:
+            if type(keywords) is not list:
+                keywords = [keywords]
+            for keyword in keywords:
+                posts = Post.objects.filter(description__icontains=keyword) | Post.objects.filter(title__icontains=keyword)
+        
+        if labels is not None:
+            if type(labels) is not list:
+                labels = [labels]
+            for label in labels:
+                posts = posts.filter(labels__labelName=label)
+
+        data = {"posts" : []}
+        for post in posts:
+            data["posts"].append(post.as_dict())
+        
+        return JsonResponse(data, status=200)
