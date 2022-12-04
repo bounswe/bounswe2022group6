b68994fb11a33938b911183efb2bc2e8b7223c7a
İhsan Mert Atalay <imrtatalay_111@hotmail.com>
2022-12-04 20:57:17 +0300

    home page colorized and designed, logo placed , fav icon added

--- Application/Frontend/src/pages/Home.js
+++ Application/Frontend/src/pages/Home.js
@@ -8,7 +8,8 @@ import MessageBox from "../components/MessageBox";
 import  Posts from "../components/ForumPost";
 import { BsSearch, BsFilter } from "react-icons/bs";
 import { FiDelete } from "react-icons/fi";
-
+import Logo from '../assets/fav.png'
+import Image from 'react-bootstrap/Image'
 
 
 const labels = [
@@ -88,7 +89,7 @@ export default function Home() {
   };
 
   return (
-    <div className="text-center">
+    <div className={styles.body} >
      {
      isGuestUser ? <div>
         <Link to="/login">
@@ -115,7 +116,11 @@ export default function Home() {
         </button>
       </Link></div>
       }
-      <h5 className="main-title home-page-title">MEDI SHARE</h5>
+      
+      <h5 className="main-title home-page-title" style={{textShadow: "1px 1px #000000 "}}>
+      <Image src={Logo} style={{width:"70px"}}></Image>
+        <span style={{color:"#dde296"}}>Medi</span><span style={{color:"#95c3b3"}}>Share</span></h5>
+      
       <div>
         {" "}
         {isLoggedout && (
@@ -142,12 +147,14 @@ export default function Home() {
           <div
             className="searchBar"
             style={{
-              backgroundColor: "white",
+              backgroundColor: "#dde296",
+              marginTop:"15px",
               marginBlockEnd: "20px",
               padding: "10px",
               display: "flex",
               alignItems: "center",
               borderRadius: "5px",
+
             }}
           >
             <input
@@ -159,6 +166,7 @@ export default function Home() {
                 height: "40px",
                 fontSize: "19px",
                 borderRadius: "5px 0px 0px 5px",
+                background:"lightgoldenrodyellow"
               }}
             />
             <BsSearch
@@ -183,20 +191,12 @@ export default function Home() {
           </div>
 
           <div
-            className="labelsSide"
-            style={{
-              display: "inline-block",
-              padding: "10px",
-              backgroundColor: "white",
-              width: "100%",
-              borderRadius: "5px",
-            }}
+            className={styles.labelsSide}
           >
             <h3
               style={{
                 display: "flex",
                 alignContent: "left",
-                color: "#383838",
               }}
             >
               Labels
@@ -208,6 +208,8 @@ export default function Home() {
                 border: "1px solid gray",
                 marginBlockEnd: "5px",
                 width: "100%",
+                background:"lightgoldenrodyellow"
+
               }}
             />
             <div
@@ -240,13 +242,13 @@ export default function Home() {
             <hr style={{ margin: "5px" }} />
             <div style={{ textAlign: "left" }}>
               {addedlabels.length === 0 && (
-                <div style={{ fontStyle: "italic", color: "gray" }}>
+                <div style={{ fontStyle: "italic",opacity:"0.5"  }}>
                   {" "}
                   add label to filter
                 </div>
               )}
               {addedlabels.length > 0 && (
-                <div style={{ color: "gray" }}>Added Labels</div>
+                <div style={{ }}>Added Labels</div>
               )}
 
               {addedlabels.map((label) => (
@@ -289,8 +291,8 @@ export default function Home() {
                 <div
                   className={styles.filter}
                   style={{
-                    background: "palegreen",
-                    color: "#333333",
+                    background: "#95c3b3",
+                    color: "#0f7375",
                     border: "1px solid gray",
                     padding: "2px 10px",
                     borderRadius: "5px",
@@ -305,9 +307,9 @@ export default function Home() {
                 <div
                   className={styles.filter}
                   style={{
-                    background: "PaleTurquoise",
+                    background: "#95c3b3",
                     border: "1px solid gray",
-                    color: "#333333",
+                    color: "#0f7375",
                     padding: "2px 10px",
                     borderRadius: "5px",
                     margin: " 3px",
