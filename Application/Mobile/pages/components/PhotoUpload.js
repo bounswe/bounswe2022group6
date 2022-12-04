28dc0a0aa145105b0fde36a0bf2c9e42f36784e6
BPamukcuoglu <bedirhanpamukcuoglu@gmail.com>
2022-12-04 19:45:26 +0300

    file upload is done

---
+++ Application/Mobile/pages/components/PhotoUpload.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { View, Text, Button, Image } from 'react-native';
+import { launchImageLibrary } from 'react-native-image-picker';
+
+const options = {
+    selectionLimit: 1
+};
+
+function S3StorageUpload({setPhotoUrl}) {
+
+
+    let photo = "";
+    let url = ""
+    const openPicker = async () => {
+        await launchImageLibrary(options, (response) => {
+            photo = response.assets[0]
+        });
+
+        var formdata = new FormData();
+        formdata.append("key", "00002649a409accae5c195b75ab1d39f");
+        formdata.append("media", {
+            name: photo.fileName,
+            type: photo.type,
+            uri: Platform.OS === 'ios' ?
+                photo.uri.replace('file://', '')
+                : photo.uri,
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+        fetch("https://thumbsnap.com/api/upload", requestOptions)
+        .then(response => response.text())
+        .then(result => {url=JSON.parse(result).data.media; setPhotoUrl(url)})
+        .catch(error => console.log('error', error));
+
+    }
+    return (
+        <View>
+            <Button onPress={openPicker} title="Open image picker"></Button>
+            <Image
+                source={{ uri: url }}
+                style={{ height: 50, width: 50 }} />
+        </View>
+    )
+}
+
+export default S3StorageUpload; 
