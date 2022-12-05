4cb106cda8e08138f0d6fd2b4edefe697bf5ca9a
Bedirhan Pamukçuoğlu <67734880+BPamukcuoglu@users.noreply.github.com>
2022-12-05 10:45:23 -0800

    Revert "Mobile/create post"

--- Application/Mobile/pages/components/PhotoUpload.js
+++ Application/Mobile/pages/components/PhotoUpload.js
@@ -1,30 +1,21 @@
 import React, { useState } from 'react';
 import { View, Text, Button, Image } from 'react-native';
 import { launchImageLibrary } from 'react-native-image-picker';
-import LoadingDisplay from './LoadingDisplay';
 
 const options = {
     selectionLimit: 1
 };
 
-function S3StorageUpload({ setPhotoUrl }) {
+function S3StorageUpload({setPhotoUrl}) {
+
 
     let photo = "";
     let url = ""
-
-    const [loading, setLoading] = useState(false);
-
     const openPicker = async () => {
-        setLoading(true)
         await launchImageLibrary(options, (response) => {
             photo = response.assets[0]
         });
 
-        await sendPicture()
-
-    }
-
-    const sendPicture = async () => {
         var formdata = new FormData();
         formdata.append("key", "00002649a409accae5c195b75ab1d39f");
         formdata.append("media", {
@@ -41,18 +32,18 @@ function S3StorageUpload({ setPhotoUrl }) {
             redirect: 'follow'
         };
 
-        await fetch("https://thumbsnap.com/api/upload", requestOptions)
-            .then(response => response.text())
-            .then(result => { url = JSON.parse(result).data.media; console.log("****", JSON.parse(result).data.media); setLoading(false); setPhotoUrl("https://thumbsnap.com/i/NNL4NH3k.jpg") })
-            .catch(error => console.log('error', error));
+        fetch("https://thumbsnap.com/api/upload", requestOptions)
+        .then(response => response.text())
+        .then(result => {url=JSON.parse(result).data.media; setPhotoUrl(url)})
+        .catch(error => console.log('error', error));
 
-        setLoading(false)
     }
     return (
         <View>
-            {loading ? <LoadingDisplay /> :
-                <Button color="#0f7375" onPress={openPicker} title="Open image picker"></Button>
-            }
+            <Button onPress={openPicker} title="Open image picker"></Button>
+            <Image
+                source={{ uri: url }}
+                style={{ height: 50, width: 50 }} />
         </View>
     )
 }
