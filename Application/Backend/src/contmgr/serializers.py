75ced0437efdbc7c9599aca66663fac25f9598c9
yusuferdemnacar <yusuferdemnacar@gmail.com>
2022-12-25 16:08:08 +0300

    Updated Get Request Response For Post, Comment, and Label
    The response to the get request of the Post, Comment, and Label are
    updated to include the user information in a uniform way with the
    profile endpoint

--- Application/Backend/src/contmgr/serializers.py
+++ Application/Backend/src/contmgr/serializers.py
@@ -1,15 +1,30 @@
 from rest_framework import serializers
 from .models import *
+from ..accmgr.serializers import RegisteredUserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
+
+    owner = RegisteredUserSerializer(read_only=True)
     class Meta:
         model = Post
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['comments'] = [CommentSerializer(comment).data for comment in instance.children_comments.all()]
+        data['result_vote'] = len(instance.upvoted_users.all()) - len(instance.downvoted_users.all())
+        return data
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['comments'] = [CommentSerializer(comment).data for comment in instance.children_comments.all()]
+        data['result_vote'] = len(instance.upvoted_users.all()) - len(instance.downvoted_users.all())
+        return data
 
 class LabelSerializer(serializers.ModelSerializer):
     class Meta:
