d762e4b7791d75bd18e72327929965c14fac57b4
araldortogul <araldortogul@gmail.com>
2022-12-26 17:45:27 +0300

    Delete Account Feature Implemented
    Users can delete their accounts now.

--- Application/Mobile/pages/profileAPI.js
+++ Application/Mobile/pages/profileAPI.js
@@ -1,23 +1,31 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BACKEND_URL } from '@env'
 
+// HTTP requests with the endpoint "profile/":
+
+// Edit profile endpoint (POST method)
+// changedData: object containing only the changed data when editing a profile
 export const editProfileRequest = async (changedData) => {
+    // Add token to header
     const token = await AsyncStorage.getItem("token")
-    var myHeaders = new Headers();
+    var myHeaders = new Headers()
     myHeaders.append("Authorization", "Token " + token);
 
-    var formdata = new FormData();
+    var formdata = new FormData(); // Form data (required for POST method)
 
+    // Append each property of changedData to formdata
     Object.keys(changedData).forEach((key) => {
         formdata.append(key, changedData[key])
     });
 
+    // Request options (POST request)
     const requestOptions = {
         method: 'POST',
         body: formdata,
         headers: myHeaders,
     };
     
+    // Send request
     try {
         const response = await fetch(BACKEND_URL + "profile/", requestOptions)
         const json = await response.json()
@@ -26,3 +34,27 @@ export const editProfileRequest = async (changedData) => {
         console.log("Error: ", error)
     }
 }
+
+// Delete profile endpoint (DELETE method)
+export const deleteProfileRequest = async () => {
+
+    // Add token to header
+    const token = await AsyncStorage.getItem("token")
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Token " + token);
+
+    // Request options
+    const requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders
+    }
+
+    // Send request
+    try {
+        const response = await fetch(BACKEND_URL + 'profile/', requestOptions)
+        const json = await response.json()
+        return json
+    } catch(error) {
+        console.log("Error: ", error)
+    }
+} 
